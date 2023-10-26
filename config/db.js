8f34d3d1083ab456c244e67aaf3d97ea793073ab
jaspreet kaur <jk739405@gmail.com>
2023-10-26 00:02:54 -0400

    my first page

---
+++ config/db.js
@@ -0,0 +1,7 @@
+/* //Assignment: Assignment 2 Express Portfolio – Authentication 
+// Laura Amangeldiyeva - student id: 301167661 
+// Date: 2022/02/15 */
+module.exports =
+{
+    "URI": "mongodb+srv://kaurjass55588:<password>@cluster0.hrzu0lc.mongodb.net/"
+}
